913f4a80e8197ff3ae423ef764487284e68cc7bb
Michael Ehrenreich <michael.ehrenreich@nb-mehrenreich.xing.hh>
2019-01-07 12:56:57 +0100

    refactored preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-mkdir -p files/var_lib_mysql \
-  files/etc_icinga2/conf.d \
-  files/var_lib_icinga2 \
-  files/var_lib_php_sessions \
-  files/etc_icingaweb2 \
-  files/etc_apache_ssl
+DIRS="files/var_lib_mysql
+files/etc_icinga2/conf.d
+files/var_lib_icinga2
+files/var_lib_php_sessions
+files/etc_icingaweb2
+files/etc_apache_ssl"
 
-touch files/etc_icinga2/conf.d/hostgroups.conf \
-  files/etc_icinga2/conf.d/hosts.conf \
-  files/etc_icinga2/conf.d/services.conf
+FILES="files/etc_icinga2/conf.d/hostgroups.conf
+files/etc_icinga2/conf.d/hosts.conf
+files/etc_icinga2/conf.d/services.conf"
+
+for dir in $DIRS ; do
+  test -d $dir || mkdir -p $dir
+done
+
+for file in $FILES ; do
+  test -f $file || touch $file
+done
